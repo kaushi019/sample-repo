dfa69c289ba990563b7c5192b9f38c9c085e3344
kaushi019 <kaushikveligatla@gmail.com>
2020-07-24 11:21:13 +0530

    added css and js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app=express();
 
 var PORT = process.env.PORT || 3500;
 
+//locate directory of the css/js file
+app.use(express.static(__dirname+'/frontend'));
+
 app.get('/',function(req,res){
-    res.send('This is the beginning....Lets start the course...');
     res.sendFile(__dirname+'/frontend/html/index.html');
 });
 
 app.listen(PORT, function(){
     console.log("http://localhost:"+PORT);
 });
-
-//ctrl+c for terminating server.
